f5927d23b4ca1d3f7e3c6e19d9bb04ffed625de5
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-03-23 23:05:37 +0100

    WIP: Adding in send method

--- lightburn/ruida.py
+++ lightburn/ruida.py
@@ -1,4 +1,24 @@
+import time
+import logging
+from enum import Enum
+from socket import socket, AF_INET, SOCK_DGRAM
+
+logger = logging.getLogger(__name__)
+
 MAGIC = 0x88
+
+MSG_ACK = 0xCC
+MSG_ERROR = 0xCD
+
+
+class RuidaCommand(Enum):
+    GET_RUN_TIME = "890bda000411"
+
+    def __init__(self, value):
+        value = bytearray.fromhex(value)
+        self.bytes = value
+        self.checksum = value[:2]
+        self.command = value[2:]
 
 
 def unswizzle(b):
@@ -29,16 +49,41 @@ def swizzle(b):
     return b
 
 
-with open("commands.txt") as f1, open("responses.txt") as f2:
-    count = 0
-    while True:
-        count += 1
-        command = bytearray.fromhex(f1.readline())
-        ack = bytearray.fromhex(f2.readline())
-        data = bytearray.fromhex(f2.readline())
-        if not (command and ack and data):
-            break
-        print(f"\nCount: {count}")
-        print("command: " + bytearray([unswizzle(b) for b in command]).hex())
-        print("ack:     " + bytearray([unswizzle(b) for b in ack]).hex())
-        print("data:    " + bytearray([unswizzle(b) for b in data]).hex())
+class RuidaCommunicator:
+    NETWORK_TIMEOUT = 3000
+    INADDR_ANY_DOTTED = '0.0.0.0'  # bind to all interfaces.
+    SOURCE_PORT = 40200  # Receive port
+    DEST_PORT = 50200  # Ruida Board
+    MTU = 1470  # max data length per datagram (minus checksum)
+
+    def __init__(self, host, dest_port=DEST_PORT, recv_port=SOURCE_PORT):
+        self.sock = socket(AF_INET, SOCK_DGRAM)
+        self.sock.bind((self.INADDR_ANY_DOTTED, recv_port))
+        self.sock.connect((host, dest_port))
+        self.sock.settimeout(self.NETWORK_TIMEOUT * 0.001)
+
+    def send(self, ary, retry=False):
+        while True:
+            self.sock.send(ary)
+            try:
+                data = self.sock.recv(8)  # timeout raises an exception
+                d0 = data[0]
+            except Exception:
+                logger.exception("Failed to receive from socket")
+                break
+            if len(data) == 0:
+                logger.warning("Received nothing (empty)")
+                break
+            if unswizzle(d0) == MSG_ERROR:
+                logger.warning("Checksum error")
+                if retry:
+                    logger.info("Retrying...")
+                    time.sleep(0.5)
+                else:
+                    raise IOError("Checksum error")
+            elif unswizzle(d0) == MSG_ACK:
+                logger.debug("Received ACK")
+                break
+            else:
+                logger.info(f"Unknown response {d0:02x}")
+                break
